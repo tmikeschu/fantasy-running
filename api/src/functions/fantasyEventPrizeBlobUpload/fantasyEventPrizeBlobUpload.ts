40f3510dca47b43ce60052f3a0773e49a46e916e
Mike Schutte <tmikeschutte@gmail.com>
2024-01-07 23:06:46 -0500

    catch

--- api/src/functions/fantasyEventPrizeBlobUpload/fantasyEventPrizeBlobUpload.ts
+++ api/src/functions/fantasyEventPrizeBlobUpload/fantasyEventPrizeBlobUpload.ts
@@ -17,46 +17,53 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     return { statusCode: 401 }
   }
 
-  return match(event)
-    .with({ httpMethod: 'POST' }, async () => {
-      const rawBody = event.isBase64Encoded
-        ? Buffer.from(event.body, 'base64').toString('utf-8')
-        : event.body
-      const body = JSON.parse(rawBody) as HandleUploadBody
-      const location = new URL(event.headers.referer)
-      // A request object is required for handleUpload, mainly to extract headers
-      const request = new Request(`${location.origin}/${event.path}`, {
-        headers: new Headers(event.headers),
-      })
-
-      return handleUpload({
-        token: process.env.BLOB_READ_WRITE_TOKEN,
-        body,
-        request,
-        onBeforeGenerateToken: async (
-          _pathname: string,
-          _clientPayload?: string
-        ) => {
-          uploadLogger.warn('.............before generate token...............')
-          return {
-            allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
-          }
-        },
-        onUploadCompleted: async () => {
-          uploadLogger.error('Blob uploaded')
-        },
-      })
-        .then((body) => ({ statusCode: 201, body }))
-        .catch((error) => {
-          uploadLogger.error({ message: error.message }, 'Blob upload failed')
-          return {
-            statusCode: 400,
-            body: { error: (error as Error).message },
-          }
+  try {
+    return match(event)
+      .with({ httpMethod: 'POST' }, async () => {
+        const rawBody = event.isBase64Encoded
+          ? Buffer.from(event.body, 'base64').toString('utf-8')
+          : event.body
+        const body = JSON.parse(rawBody) as HandleUploadBody
+        const location = new URL(event.headers.referer)
+        // A request object is required for handleUpload, mainly to extract headers
+        const request = new Request(`${location.origin}/${event.path}`, {
+          headers: new Headers(event.headers),
         })
-    })
-    .otherwise(() => {
-      uploadLogger.error('Other failure')
-      return { statusCode: 404 }
-    })
+
+        return handleUpload({
+          token: process.env.BLOB_READ_WRITE_TOKEN,
+          body,
+          request,
+          onBeforeGenerateToken: async (
+            _pathname: string,
+            _clientPayload?: string
+          ) => {
+            uploadLogger.warn(
+              event,
+              '.............before generate token...............'
+            )
+            return {
+              allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+            }
+          },
+          onUploadCompleted: async () => {
+            uploadLogger.error('Blob uploaded')
+          },
+        })
+          .then((body) => ({ statusCode: 201, body }))
+          .catch((error) => {
+            uploadLogger.error({ message: error.message }, 'Blob upload failed')
+            return {
+              statusCode: 400,
+              body: { error: (error as Error).message },
+            }
+          })
+      })
+      .otherwise(() => {
+        uploadLogger.error('Other failure')
+        return { statusCode: 404 }
+      })
+  } catch (e) {
+    uploadLogger.error(e.message, 'ERROR')
+  }
 }
