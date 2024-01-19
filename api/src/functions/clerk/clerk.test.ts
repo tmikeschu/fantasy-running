c12ba7e1f36baab48b02989f52cdd5fbc3724284
Mike Schutte <tmikeschutte@gmail.com>
2024-01-18 21:55:52 -0500

    base64 test

--- api/src/functions/clerk/clerk.test.ts
+++ api/src/functions/clerk/clerk.test.ts
@@ -36,6 +36,33 @@ describe('clerk function', () => {
     expect(user.avatarUrl).toEqual('https://example.com/avatar.png')
   })
 
+  test('converts base64 string', async () => {
+    const payload = {
+      type: 'user.created',
+      data: {
+        id: 'external',
+        email_addresses: [{ email_address: 'a@b.c' }],
+        profile_image_url: 'https://example.com/avatar.png',
+      },
+    } as ClerkPayload
+
+    const event = mockSignedWebhook({
+      payload,
+      signatureType: 'base64Sha256Verifier',
+      signatureHeader: 'svix-signature',
+      secret: process.env.CLERK_WH_SECRET.slice(6),
+      isBase64Encoded: true,
+    })
+
+    expect(await db.user.count()).toEqual(0)
+    const result = await handler(event)
+
+    expect(result.statusCode).toBe(200)
+    const user = await db.user.findFirst()
+    expect(user.externalId).toEqual('external')
+    expect(user.avatarUrl).toEqual('https://example.com/avatar.png')
+  })
+
   test('upserts a user', async () => {
     const existingUser = await db.user.create({
       data: { externalId: 'external', email: 'a@b.c' },
